afd6feaea0a5847e954eec476eac1a871bd49dd2
Mehreen Rahman <mimirahman2013@gmail.com>
2017-09-25 00:33:00 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,50 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  data[season].each do |hash|
+    if hash["status"] == "Winner"
+      return hash["name"].split[0]
+  end
+end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant_hash|
+    if contestant_hash["occupation"] == occupation
+      return contestant_hash["name"]
+    end
+  end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, contestant|
+    contestant.each do |contestant_hash|
+      if contestant_hash["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+  return count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestant|
+    contestant.each do |contestant_hash|
+      if contestant_hash["hometown"]==hometown
+        return contestant_hash["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age = 0
+  count = 0
+  data[season].each do |contestant_hash|
+      age += contestant_hash["age"].to_i
+      count += 1
+    end
+  return (age/count.to_f).round(0)
 end
